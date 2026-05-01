8d419c674dd9223712063144f44387b466d85818
poornaD2003 <poornadiwyanjana@gmail.com>
2026-05-01 21:18:21 +0530

    added doctor page

---
+++ Project 01/doctorPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project_01
+{
+    public partial class doctorPage : Form
+    {
+        public doctorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
