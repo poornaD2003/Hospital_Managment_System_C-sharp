11688d5e50995ccef50cc7231020869c1dd49634
poornaD2003 <poornadiwyanjana@gmail.com>
2026-05-01 21:19:30 +0530

    updated

--- Project 01/Admin.cs
+++ Project 01/Admin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,7 +13,50 @@ namespace Project_01
         }
         public override void openPage()
         {
-           
+            try
+            {
+                using (SqlConnection conn = dbConnection.GetConnection())
+                {
+                    conn.Open();
+
+                    string sql = "SELECT password FROM [user] WHERE userName = @username";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@username", this.getUsername());
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string storedHash = reader["password"].ToString();
+                               
+
+
+                                bool isValid = BCrypt.Net.BCrypt.Verify(getPassword(), storedHash);
+
+                                if (isValid)
+                                {
+                                    adminPage adminDashboard = new adminPage();
+                                    adminDashboard.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Invalid password. Please try again.");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Patient record not found.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database Error: " + ex.Message);
+            }
         }
     }
 }
