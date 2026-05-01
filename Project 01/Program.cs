e15b35175f44d5f8ddeeac59bacd8383a7f7e4dc
poornaD2003 <poornadiwyanjana@gmail.com>
2026-05-01 21:12:58 +0530

    upated

--- Project 01/Program.cs
+++ Project 01/Program.cs
@@ -11,7 +11,7 @@ namespace Project_01
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new login());
+            Application.Run(new login(new signUp(null)));
         }
     }
 }
